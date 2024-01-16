ae12b6bbb0f12f5ba64f0be2a4bab0f455af99b2
alex <al3xback@gmail.com>
2024-01-16 16:52:07 +0700

    added first commit

---
+++ spec/mainSpec.js
@@ -0,0 +1,75 @@
+import jsdom from 'jsdom';
+
+import {
+	createElementAttribute,
+	createElement,
+	createHeaderEl,
+	createCardEl,
+	createMainEl,
+	createFooterEl,
+} from '../js/util.js';
+
+const { JSDOM } = jsdom;
+
+describe('DOM', () => {
+	beforeEach(() => {
+		const { document } = new JSDOM(
+			`<!DOCTYPE html><html><body></body></html>`
+		).window;
+		global.document = document;
+	});
+
+	it("should be able to create element via 'createElement' function", () => {
+		const anchorEl = createElement('a', 'btn', 'Click me');
+		document.body.appendChild(anchorEl);
+
+		const isAnchorElExist = !!document.querySelector('.btn');
+		expect(isAnchorElExist).not.toBeNull();
+	});
+
+	it("should be able to return element attribute data via 'createElementAttribute' function", () => {
+		const hrefData = createElementAttribute(
+			'href',
+			'https://www.frontendmentor.io?ref=challenge'
+		);
+
+		const expectedHrefData = {
+			name: 'href',
+			value: 'https://www.frontendmentor.io?ref=challenge',
+		};
+
+		expect(hrefData).toEqual(expectedHrefData);
+	});
+
+	it("should be able to create header element via 'createHeaderEl' function", () => {
+		const headerEl = createHeaderEl();
+		document.body.appendChild(headerEl);
+
+		const isHeaderElExist = !!document.querySelector('header');
+		expect(isHeaderElExist);
+	});
+
+	it("should be able to create card element via 'createCardEl' function", () => {
+		const cardEl = createCardEl();
+		document.body.appendChild(cardEl);
+
+		const isCardElExist = !!document.querySelector('.card');
+		expect(isCardElExist).not.toBeNull();
+	});
+
+	it("should be able to create main element via 'createMainEl' function", () => {
+		const mainEl = createMainEl();
+		document.body.appendChild(mainEl);
+
+		const isMainElExist = !!document.querySelector('main');
+		expect(isMainElExist).not.toBeNull();
+	});
+
+	it("should be able to create footer element via 'createFooterEl' function", () => {
+		const footerEl = createFooterEl();
+		document.body.appendChild(footerEl);
+
+		const isFooterElExist = !!document.querySelector('footer');
+		expect(isFooterElExist).not.toBeNull();
+	});
+});
